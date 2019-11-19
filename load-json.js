2ef25c290fabca83be32421b9295435d081a23a3
ElementMo <869077498@qq.com>
2019-11-19 17:22:57 -0500

    Extract location data from Json, and display them on map (#6)
    * Extract location data from Json, and display them on map

--- load-json.js
+++ load-json.js
@@ -2,26 +2,44 @@ var loadedJSON = null;
 
 inputField = document.querySelector("#JSONFile");
 inputField.onchange = function () {
+    echartslayer.chart.showLoading();   // Show Loading Animation
+
     if ('files' in inputField && inputField.files.length == 1) {
         var file = inputField.files[0];
         var reader = new FileReader();
-        reader.onload = function(e)
-        {
+        reader.onload = function (e) {
             var contents = e.target.result;
-            try
-            {
+            try {
                 loadedJSON = JSON.parse(contents.toString());
             }
-            catch (e)
-            {
+            catch (e) {
                 alert("Unable to parse JSON");
             }
             console.log(loadedJSON);
+
+            try {
+                var locationData = [];
+                for (var i = 0; i < loadedJSON.locations.length; i++) {
+                    locationData.push([loadedJSON.locations[i].longitudeE7 * 0.0000001, loadedJSON.locations[i].latitudeE7 * 0.0000001]);
+                }
+                newOption = {
+                    series: [
+                        {
+                            data: locationData
+                        }
+                    ]
+                }
+                echartslayer.chart.setOption(newOption);    // update option
+                echartslayer.chart.hideLoading();           // Hide Loading Animation
+            }
+            catch (e) {
+                alert("File content error");
+                echartslayer.chart.hideLoading();
+            }
         }
         reader.readAsText(file, "UTF-8");
     }
-    else
-    {
+    else {
         alert("Failed to load file");
     }
 }
--- load-map.js
+++ load-map.js
@@ -1,3 +1,5 @@
+var echartslayer;
+
 var map = new mapboxgl.Map(
     {
         container: "container",
@@ -12,10 +14,10 @@ var map = new mapboxgl.Map(
 );
 
 map.addControl(new mapboxgl.NavigationControl());
+
 map.on(
     "load",
-    function () 
-    {
+    function () {
         map.addSource(
             "google.tile",
             { "type": "raster", "tiles": ['https://www.google.com/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}'], "tileSize": 256 }
@@ -25,7 +27,26 @@ map.on(
             "type": "raster",
             "source": "google.tile"
         })
-
-        var echartslayer = new EchartsLayer(map);
+        option = {
+            backgroundColor: '#101520BB',
+            GLMap: {},
+            series: [
+                {
+                    data: [],
+                    type: "scatter",
+                    coordinateSystem: 'GLMap',
+                    symbolSize: 4,
+                    itemStyle: {
+                        normal: {
+                            opacity: 0.5,
+                            color: '#20AADD'
+                        }
+                    },
+                    blendMode: 'screen'
+                }
+            ]
+        }
+        echartslayer = new EchartsLayer(map);
+        echartslayer.chart.setOption(option);
     }
 );
