94259fe48c5f6ae51b73f730945363d72c0d23b9
Antonio Woods <antonio95woods@gmail.com>
2019-11-29 18:13:55 -0500

    Add map selector dropdown

--- load-map.js
+++ load-map.js
@@ -3,26 +3,24 @@ var echartslayer;
 // My token (not public token)
 mapboxgl.accessToken = "pk.eyJ1IjoiZWxlbWVudG1vIiwiYSI6ImNrM2Y4dm55MjAwNnczbG80M2hrZ3Q3dzEifQ._AmTHcuM701F6x6iBmt_yA";
 
-var map = new mapboxgl.Map(
-    {
-        container: "container",
-        // style: {
-        //     "version": 8,
-        //     "sources": {},
-        //     "layers": []
-        // },
-        style: "mapbox://styles/mapbox/dark-v10",
-        center: [-77.045, 38.896],
-        zoom: 13
-    }
-);
+var map = new mapboxgl.Map({
+    container: "container",
+    // style: {
+    //     "version": 8,
+    //     "sources": {},
+    //     "layers": []
+    // },
+    style: "mapbox://styles/mapbox/dark-v10",
+    center: [-77.045, 38.896],
+    zoom: 13
+});
 
 map.addControl(new mapboxgl.NavigationControl());
 
 
 map.on(
     "load",
-    function () {
+    function() {
         // map.addSource(
         //     "google.tile",{
         //         "type": "raster",
@@ -38,23 +36,21 @@ map.on(
         option = {
             backgroundColor: '#10152077',
             GLMap: {},
-            series: [
-                {
-                    type: "scatterGL",
-                    progressive: 1e6,
-                    coordinateSystem: 'GLMap',
-                    symbolSize: 2,
-                    blendMode: 'lighter',
-                    large: true,
-                    itemStyle: {
-                        normal: {
-                            color: '#20AADD'
-                        },
-                        silent: true,
+            series: [{
+                type: "scatterGL",
+                progressive: 1e6,
+                coordinateSystem: 'GLMap',
+                symbolSize: 2,
+                blendMode: 'lighter',
+                large: true,
+                itemStyle: {
+                    normal: {
+                        color: '#20AADD'
                     },
-                    data: new Float32Array()
-                }
-            ]
+                    silent: true,
+                },
+                data: new Float32Array()
+            }]
         }
         echartslayer = new EchartsLayer(map);
         echartslayer.chart.setOption(option);
